3beb17d8b9abe9d3a181ae16b4666ceed9484871
thalia13 <thalia13vf@gmail.com>
2022-10-05 09:55:06 -0300

    modificando testes

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Employee {
     private String name;
     private int idade;
@@ -21,5 +23,26 @@ public class Employee {
 
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return idade == employee.idade && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", idade=" + idade +
+                '}';
     }
 }
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,14 +1,13 @@
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,12 +16,22 @@ public class EmployeeTest {
     @InjectMocks
     private EmployeeService employeeService;
 
+    private static List<Employee> employeesList;
+
+    @BeforeAll
+    public static void setUp(){
+        employeesList = Arrays.asList(new Employee("Max", 17),
+                new Employee("Sepp", 18),
+                new Employee("Nina", 15),
+                new Employee("Mike", 51));
+    }
+
     @Test
     @DisplayName("Get a list with all employees")
     public void searchAllEmployees() {
         List<Employee> employees = employeeService.findAllEmployees();
 
-        Assertions.assertFalse(employees.isEmpty());
+        Assertions.assertEquals(employees, employeesList);
     }
 
     @Test
@@ -30,19 +39,20 @@ public class EmployeeTest {
     public void getEmployeesWhoAre18OrOlder() {
         List<Employee> employees18OrOlder = employeeService.findEmployees18OrOlder();
 
-        Assertions.assertNotNull(employees18OrOlder);
+        Assertions.assertEquals(employees18OrOlder.get(0).getName(), employeesList.get(1).getName().toUpperCase());
+        Assertions.assertEquals(employees18OrOlder.get(1).getName(), employeesList.get(3).getName().toUpperCase());
+        Assertions.assertTrue(employees18OrOlder.get(0).getIdade() >=18);
+        Assertions.assertTrue(employees18OrOlder.get(1).getIdade() >= 18);
     }
 
     @Test
     @DisplayName("The shop owner wants to see the employees list sorted by name")
     public void getEmployeesWhoAre18OrOlderSortByName(){
         List<Employee> employees18OrOlderSortByName = employeeService.findEmployees18OrOlder();
-        List<String> employessName = employees18OrOlderSortByName.stream()
-                .map(Employee::getName)
-                .map(String::toLowerCase)
-                .toList();
 
-        Assertions.assertLinesMatch(Arrays.asList("mike", "sepp"), employessName);
+        Assertions.assertEquals(employees18OrOlderSortByName.get(0).getName(), employeesList.get(1).getName().toUpperCase());
+        Assertions.assertEquals(employees18OrOlderSortByName.get(1).getName(), employeesList.get(3).getName().toUpperCase());
+
     }
 
     @Test
@@ -50,13 +60,8 @@ public class EmployeeTest {
     public void getEmployeesWhoAre18OrOlderSortByNameCapitelized(){
         List<Employee> employees18OrOlderSortByNameAndCapitelized = employeeService.findEmployees18OrOlder();
 
-        List<String> employessName = employees18OrOlderSortByNameAndCapitelized
-                .stream()
-                .map(Employee::getName)
-                .map(String::toUpperCase)
-                .toList();
-
-        Assertions.assertLinesMatch(Arrays.asList("MIKE", "SEPP"), employessName);
+        Assertions.assertEquals(employees18OrOlderSortByNameAndCapitelized.get(0).getName().toUpperCase(),"SEPP");
+        Assertions.assertEquals(employees18OrOlderSortByNameAndCapitelized.get(1).getName().toUpperCase(),"MIKE");
     }
 
     @Test
@@ -64,13 +69,8 @@ public class EmployeeTest {
     public void getEmployeesWhoAre18OrOlderSortByNameCapitelizedDescending(){
         List<Employee> employees18OrOlderSortByNameAndCapitelized = employeeService.findEmployees18OrOlder();
 
-        List<String> employessName = employees18OrOlderSortByNameAndCapitelized
-                .stream()
-                .map(Employee::getName)
-                .map(String::toUpperCase)
-                .sorted(Comparator.reverseOrder())
-                .toList();
 
-        Assertions.assertLinesMatch(Arrays.asList("SEPP", "MIKE"), employessName);
+        Assertions.assertEquals(employees18OrOlderSortByNameAndCapitelized.get(0).getName(), "SEPP");
+        Assertions.assertEquals(employees18OrOlderSortByNameAndCapitelized.get(1).getName(), "MIKE");
     }
 }
